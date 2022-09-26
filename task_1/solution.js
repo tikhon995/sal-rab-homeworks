337a0fffe449c21860d47f6edcaf33de991d1104
Tikhon Shulga <tikhon-94@yandex.ru>
2022-09-26 21:18:11 +0300

    commit hometask

--- task_1/solution.js
+++ task_1/solution.js
@@ -2,6 +2,17 @@ function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
 
+    1 let message;
+    1 message = productName;
+    1 message = message + " ";
+    1 message = message + " за";
+    1 message = message + " ";
+    1 message = message + " " + productPrice;
+    1 message = message + " ";
+    1 message = message + " теперь в корзине!";
+    console.log(message);
+
+
     // Задание №1.1. Оповещение о добавлении в корзину
 
     // создайте переменную message
@@ -23,6 +34,10 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue;
+    1 newValue = oldValue;
+    1 newValue = newValue + 1;
+   
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
@@ -36,6 +51,12 @@ function addInCartCountChange(value) {
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
+
+    let newSum;
+    1 newSum = oldSum + difference;
+    1 newSumText;
+    1 newSumText = newSum + " ₽";
+    1 newSumText = `${newSum} ₽`;
 
     // Задание №1.3. Увеличение суммы в корзине
 
