bbd3d4168b28dd0c8d95956258548ea690d8ef5c
Tikhon Shulga <tikhon-94@yandex.ru>
2022-10-01 18:05:34 +0300

    18:05 2022-10-01

--- task_2/solution.js
+++ task_2/solution.js
@@ -2,7 +2,14 @@ function calcShipping(sum, min, shipping) {
     let productsSum = sum; // сумма в корзине
     let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
     let shippingPrice = shipping; // стоимость доставки
-
+    
+    let shippingSum;
+    if (productsSum == 0 || productsSum >= freeShippingMinSum){
+        shippingSum = 0;
+      }  
+    if((productsSum > 0) && (productsSum < freeShippingMinSum)){
+        shippingSum =  shippingPrice;
+    }
     // Задание №2.1. Рассчитать доставку
 
     // создайте переменную shippingSum
@@ -26,6 +33,10 @@ function calcDiscount(sum, min, discount) {
     let discountMinSum = min; // минимальная цена для скидки
     let discountPart = discount; // величина скидки в процентах
 
+    let discountSum;
+    if(productsSum >= discountMinSum){
+        discountSum = discountPart / 100 * productsSum;
+    }else(discountSum == 0);
     // Задание №2.2. Рассчитать скидку
 
     // создайте переменную discountSum
@@ -43,6 +54,9 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     let productsSum = sum;
     let discountSum = calcDiscount(sum, discountMinSum, discountPart);
 
+    let totalSum;
+    totalSum = productsSum;
+    totalSum = totalSum - discountSum;
     // Задача №2.3. Рассчитать скидки и доставку в корзине
 
     // создайте переменную totalSum
@@ -51,6 +65,12 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // уменьшите totalSum на discountSum
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
+
+    totalSum = totalSum + shippingSum;
+    
+    let freeShipping;
+    freeShipping = !shippingSum;
+
 
     // прибавьте к totalSum значение shippingSum
 
